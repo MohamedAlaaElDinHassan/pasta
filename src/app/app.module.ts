c0c0e54154f18a647053243ee643ae4c05188449
Mohamed Ibrahim <mohamed.ibr1994@gmail.com>
2017-10-22 10:48:09 +0200

    Adding StorageModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,69 +1,72 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { SearchPage } from '../pages/search/search';
-import { ListPage } from '../pages/list/list';
-import { RecipetsPage } from '../pages/recipets/recipets';
-import { RecipetPage } from '../pages/recipet/recipet';
-import { BrowsePage } from '../pages/browse/browse';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {SearchPage} from '../pages/search/search';
+import {ListPage} from '../pages/list/list';
+import {RecipetsPage} from '../pages/recipets/recipets';
+import {RecipetPage} from '../pages/recipet/recipet';
+import {BrowsePage} from '../pages/browse/browse';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 import {MailPage} from "../pages/mail/mail";
-import { LoginPage } from '../pages/login/login';
-import { WishlistPage } from '../pages/wishlist/wishlist';
-import { InstallPage } from '../pages/install/install';
-import { PostsProvider } from '../providers/posts/posts';
+import {LoginPage} from '../pages/login/login';
+import {WishlistPage} from '../pages/wishlist/wishlist';
+import {InstallPage} from '../pages/install/install';
+import {PostsProvider} from '../providers/posts/posts';
 import {HttpModule} from "@angular/http";
-import { ShoppingListProvider } from '../providers/shopping-list/shopping-list';
+import {ShoppingListProvider} from '../providers/shopping-list/shopping-list';
 import {SearchFilterPipe} from "../pipes/search-filter/search-filter";
+import {IonicStorageModule} from "@ionic/storage";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-      SearchPage,
-    ListPage,
-      RecipetsPage,
-      RecipetPage,
-      BrowsePage,
-      MailPage,
-      LoginPage,
-      WishlistPage,
-      InstallPage,
-      SearchFilterPipe
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp, {
-        backButtonText: '',
-        pageTransition: 'ios-transition'
-    }),
-      HttpModule
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-      SearchPage,
-    ListPage,
-      RecipetsPage,
-      RecipetPage,
-      BrowsePage,
-      MailPage,
-      LoginPage,
-      WishlistPage,
-      InstallPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    PostsProvider,
-    ShoppingListProvider
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        SearchPage,
+        ListPage,
+        RecipetsPage,
+        RecipetPage,
+        BrowsePage,
+        MailPage,
+        LoginPage,
+        WishlistPage,
+        InstallPage,
+        SearchFilterPipe
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp, {
+            backButtonText: '',
+            pageTransition: 'ios-transition'
+        }),
+        HttpModule,
+        IonicStorageModule.forRoot()
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        SearchPage,
+        ListPage,
+        RecipetsPage,
+        RecipetPage,
+        BrowsePage,
+        MailPage,
+        LoginPage,
+        WishlistPage,
+        InstallPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        PostsProvider,
+        ShoppingListProvider
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
